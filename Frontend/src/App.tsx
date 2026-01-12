5d55803a2e571ea767cade0e5cce3d259b07ab27
Kavin SWS <kavin@softworthsolutions.in>
2026-01-12 19:00:26 +0530

    added jobsDashboard

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,7 +1,3 @@
-import JobCardComponent from "./components/jobs/JobCardComponent"
-import JobDashboardComponent from "./components/jobs/JobDashboardComponent"
-import JobMetricsComponent from "./components/jobs/JobMetricsComponent"
-import CreateJobContainer from "./containers/CreateJobContainer"
 import JobDashboardContainer from "./containers/JobDashboardContainer"
 
 function App() {
